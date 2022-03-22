00c6619a2e341d5a71260e0d3341e9be3d74f81b
dmknapp2385 <dmknapp2385@gmail.com>
2022-03-22 12:37:10 -0700

    added all work information

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -4,10 +4,10 @@ import headShot from '../../images/head-shot.jpg';
 function About(props) {
  
   return (
-    <section className='pt-13' id='about'>
-      <div className=' ml-5 mr-5 is-flex is-justify-content-space-around level'>
-        <img className='mr-5' id='self-img' src={headShot}/>
-        <p>
+    <section className='pt-13 pb-13' id='about'>
+      <div className=' ml-6 mr-6 is-flex is-justify-content-space-around level'>
+        <img className='mr-6' id='self-img' src={headShot}/>
+        <p className='mr-6 '>
         I am an avid rock climber and adventurer. Currently exploring the wonders of HTML, CSS and JavaScript in the hopes of obtaining a remote job that will allow me to travel and climb. I have always been a problem solver and find web development to be a perfect blend of problem solving and creativity. My background before coming to web development and programming was in biology and I attended two years of college at Cornell University and Wellesley college to obtain a bachelor's degree in Biology, though after discovering rock climbing, I had no interest in pursuing a career with this degree.
         <br></br>
         I have always had a nomadic spirit and have lived all over the United States, growing up in Jackson Hole, Wyoming and Three Rivers, California where my parents worked for the National Park Service. I have a passion for all things nature and outdoor recreation. I have recently spent time in Maine, Tennessee and currently reside in Arizona. I am very curious and constantly expanding my learning and discovering new things that pique my interest.
--- src/Components/Works/index.js
+++ src/Components/Works/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import images from '../../images'
 
-
+console.log(images)
+//on hover, show popup modal with description of project
 function Works () {
     const works =[
         {
@@ -12,38 +14,38 @@ function Works () {
         },
         {
             name: 'Bored and Brews',
-            description: 'An application designed using third party REST API endpoints to defeat boredum with activites and brews. The application will generate a random activity and list of nearby breweries that users can store in a weekly calendar to defeat bordem',
-            image: '../images/bored.jpg',
-            url: 'https://song-starter-3000.herokuapp.com/',
-            github: 'https://github.com/dmknapp2385/',
+            description: 'An application designed using third party REST APIs to defeat boredum with activites and brews to enjoy after. The application generates a random activity and list of nearby breweries that users can store in a weekly calendar to defeat bordem',
+            image: 'bored.jpg',
+            url: 'https://fmatthew40.github.io/bored-and-brews/',
+            github: 'https://github.com/dmknapp2385/bored-and-brews',
         },
         {
-            name: 'Song Starter 3000',
-            description: 'An application designed to help cultivate creativity in musicians by generating random chord progressions',
-            image: '../images/song-starter.jpg',
-            url: 'https://song-starter-3000.herokuapp.com/',
-            github: 'https://github.com/dmknapp2385/Song-Starter-3000/tree/main',
+            name: 'Techy News',
+            description: 'A website that allows a user to view posts about tech. Once signed up, users can save posts, create posts and comment on other user posts',
+            image: 'tech.jpg',
+            url: 'https://warm-escarpment-95693.herokuapp.com/',
+            github: 'https://github.com/dmknapp2385/Techy-News',
         },
         {
-            name: 'Song Starter 3000',
-            description: 'An application designed to help cultivate creativity in musicians by generating random chord progressions',
-            image: '../images/song-starter.jpg',
-            url: 'https://song-starter-3000.herokuapp.com/',
-            github: 'https://github.com/dmknapp2385/Song-Starter-3000/tree/main',
+            name: 'WeatherSpot',
+            description: 'A weather application that will show the current weather conditions of a location and a five day forecast. Most recent searches are saved and displayed for quick searching',
+            image: 'weather.jpg',
+            url: 'https://dmknapp2385.github.io/WeatherSpot/',
+            github: 'https://github.com/dmknapp2385/WeatherSpot',
         },
         {
-            name: 'Song Starter 3000',
-            description: 'An application designed to help cultivate creativity in musicians by generating random chord progressions',
-            image: '../images/song-starter.jpg',
-            url: 'https://song-starter-3000.herokuapp.com/',
-            github: 'https://github.com/dmknapp2385/Song-Starter-3000/tree/main',
+            name: 'Coding Quiz',
+            description: 'A simple website used to test basic coding concepts. The quiz is timed and time is deducted for wrong answers. High scores are saved',
+            image: 'quiz.jpg',
+            url: 'https://dmknapp2385.github.io/Coding-Quiz/',
+            github: 'https://github.com/dmknapp2385/Coding-Quiz',
         },
         {
-            name: 'Song Starter 3000',
-            description: 'An application designed to help cultivate creativity in musicians by generating random chord progressions',
-            image: '../images/song-starter.jpg',
-            url: 'https://song-starter-3000.herokuapp.com/',
-            github: 'https://github.com/dmknapp2385/Song-Starter-3000/tree/main',
+            name: 'Notable',
+            description: 'A note/task making application that allows users to make lists of notes, edit and complete/delete notes',
+            image: 'notable.jpg',
+            url: 'https://arcane-ridge-53547.herokuapp.com/',
+            github: 'https://github.com/dmknapp2385/Notable',
         },
     ]    
     return(
